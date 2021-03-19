fb923e71631cd8aa31a9579e3b5eb39b6dad61ca
Harshit Bhatia <72250893+Harshit0820@users.noreply.github.com>
2021-03-19 15:18:59 +0530

    Add files via upload

---
+++ src/DBConnection/Configs.java
@@ -0,0 +1,9 @@
+package DBConnection;
+
+public class Configs {
+    protected static String dbhost = "localhost";
+    protected static String dbport = "3306";
+    protected static String dbuser = "root";
+    protected static String dbpass = "harshit0820";
+    protected static String dbname = "data";
+}
